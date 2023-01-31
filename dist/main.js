d188874f613214b0b29e09f958b95781f74289a6
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-01-30 21:46:08 -0500

    Configuration of webpack and linters

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// JavaScript source code
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbGVhZGVyYm9hcmQvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gSmF2YVNjcmlwdCBzb3VyY2UgY29kZVxyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+
+    static: './dist',
+
+  },
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+      title: 'Output Management',
+
+    }),
+
+  ],
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+      {
+
+        test: /\.json5$/i,
+
+        type: 'json',
+
+        parser: {
+
+          parse: json5.parse,
+
+        },
+
+      },
+
+    ],
+
+  },
+};
