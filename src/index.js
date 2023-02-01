0dae79371e1c5e9b06f3a41eee11116ddd1e2f3b
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-02-01 10:53:56 -0500

    Creating game with a name AlexisGame

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+/*https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/bwl5MuXx9HP3kxRHXohk/scores/ */
+const createGame = async (gameName) => {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: gameName,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    const json = await response.json();
+    console.log('name: ' + json.name + ' result: ' + json.result);
+};
